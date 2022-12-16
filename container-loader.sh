4c4a0748fe4312046287b08599f70c87ea6d6e6b
Putu Audi Pasuatmadi <audipasuatmadi@gmail.com>
2022-12-16 12:59:35 +0800

    [feature] add container loader

---
+++ container-loader.sh
@@ -0,0 +1,5 @@
+kind load docker-image audi-skripsi-lambda-event-presenter:1.2.0 --name audi-skripsi
+kind load docker-image audi-skripsi-lambda-batch-processor:1.0.1 --name audi-skripsi
+kind load docker-image audi-skripsi-lambda-identifier:1.0.1 --name audi-skripsi
+kind load docker-image audi-skripsi-lambda-level-standardizer:1.0.1 --name audi-skripsi
+kind load docker-image audi-skripsi-log-ingestor:1.1.1 --name audi-skripsi
